d7efcaa87b8498003f24992720f8f1312fe9257b
ummmmm <ummmmm@users.noreply.github.com>
2015-03-18 15:38:11 -0700

    Removed unused code from the main file

--- SVNPlugin.py
+++ SVNPlugin.py
@@ -1,13 +1,1 @@
-import sys
-import imp
-
-from .svn_plugin.svn				import SVN
-from .svn_plugin.settings 			import Settings
-from .svn_plugin.tracked_files 		import TrackedFiles
-from .svn_plugin.thread_progress 	import ThreadProgress
-from .svn_plugin.threads			import *
-from .svn_plugin.commands			import *
-
-for mod in sys.modules:
-	if 'svn_plugin' in mod:
-		imp.reload( sys.modules[ mod ] )
+from .svn_plugin.commands import *

e9376aac5b9a800aa94b121c9a90664ea4336b1b
ummmmm <ummmmm@users.noreply.github.com>
2015-03-25 10:30:23 -0700

    Commands are built using a list of arguments
    SVN commands are now constructed by using the the builtin join
    function.  A list of args are passed into run_command where they
    will be turned into a string command

--- svn_plugin/svn.py
+++ svn_plugin/svn.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 import subprocess
 import json
 import collections
+import re
 
 from .settings import Settings
 
@@ -28,62 +29,64 @@ class SVN():
 		self.valid				= True
 
 	def info( self, path ):
-		self.run_command( 'info --xml {0}' . format( shlex.quote( path ) ) )
+		self.run_command( [ 'info', '--xml', path ] )
 
 		return self.result()
 
 	def log( self, path, limit = None, revision = None ):
-		command = 'log --xml'
+		args = [ 'log', '--xml' ]
 
 		if limit is not None:
-			command += ' --limit={0}' . format( limit )
+			args.extend( [ '--limit', limit ] )
 
 		if revision is not None:
-			command += ' --revision={0}' . format( revision )
+			args.extend( [ '--revision', revision ] )
 
-		self.run_command( '{0} {1}' . format( command, shlex.quote( path ) ) )
+		args.append( path )
+
+		self.run_command( args )
 
 		return self.result()
 
 	def add( self, path ):
-		self.run_command( 'add {0}' . format( shlex.quote( path ) ) )
+		self.run_command( [ 'add', path ] )
 
 		return self.result()
 
 	def remove( self ):
-		self.run_command( 'remove {0}' . format( shlex.quote( self.path ) ) )
+		self.run_command( [ 'remove', path ] )
 
 		return self.result()
 
 	def revert( self, path ):
-		self.run_command( 'revert {0}' . format( shlex.quote( path ) ) )
+		self.run_command( [ 'revert', path ] )
 
 		return self.result()
 
 	def commit( self, path, commit_file_path ):
-		self.run_command( 'commit --file={0} {1}' . format( shlex.quote( commit_file_path ), path ) )
+		self.run_command( [ 'commit', '--file', commit_file_path, path ] )
 
 		return self.result()
 
 	def annotate( self, path, revision ):
-		self.run_command( 'annotate --revision={0} {1}' . format( revision, shlex.quote( path ) ) )
+		self.run_command( [ 'annotate', '--revision', revision, path ] )
 
 		return self.result()
 
 	def diff( self, path, revision = None, diff_tool = None ):
-		command = 'diff'
+		args 	= [ 'diff' ]
 		block	= True
 
 		if revision:
-			command += ' --revision={0}' . format( revision )
+			args.extend( [ '--revision', revision ] )
 
 		if diff_tool is not None:
 			block	= False
-			command += ' --diff-cmd={0}' . format( shlex.quote( diff_tool ) )
+			args.extend( [ '--diff-cmd', diff_tool ] )
 
-		command += ' {0}' . format( shlex.quote( path ) )
+		args.append( path )
 
-		self.run_command( command, block = block )
+		self.run_command( args, block = block )
 
 		if not block:
 			return True
@@ -91,28 +94,43 @@ class SVN():
 		return self.result()
 
 	def cat( self, path, revision = None ):
-		if revision is None:
-			self.run_command( 'cat {0}' . format( shlex.quote( path ) ) )
-		else:
-			self.run_command( 'cat --revision={0} {1}' . format( revision, shlex.quote( path ) ) )
+		args = [ 'cat' ]
+		if revision is not None:
+			args.extend( [ '--revision', revision ] )
+
+		args.append( path )
+
+		self.run_command( args )
 
 		return self.result()
 
 	def update( self, path ):
-		self.run_command( 'update {0}' . format( shlex.quote( path ) ) )
+		self.run_command( [ 'update', path ] )
 
 		return self.result()
 
 	def status( self, path ):
-		self.run_command( 'status --xml {0}' . format( shlex.quote( path ) ) )
+		self.run_command( [ 'status', '--xml', path ] )
 
 		return self.result()
 
-	def run_command( self, command, block = True ):
-		command = '{0} {1}' . format( self.binary, command )
+	def run_command( self, args, block = True ):
+		args.insert( 0, self.binary )
+
+		escaped_args = []
+
+		for arg in args:
+			arg = str( arg )
+
+			if re.search( '^[a-zA-Z0-9/_^\\-\\.:]+$', arg ) == None:
+				arg = "'{0}'" . format( arg )
+
+			escaped_args.append( arg )
+
+		command = ' ' . join( escaped_args )
 
 		if self.log_commands:
-			print( command )
+			print( 'SVN Command:', command )
 
 		if not block:
 			subprocess.Popen( command, shell = True, cwd = '/tmp' )
